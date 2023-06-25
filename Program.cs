c5b80a4435d61e28e6e17e47f9d77e0f364af37e
Lyubomyr Papizh <lyubomyr.papizh@devspiration.com>
2023-06-25 21:45:38 +0300

    Added DateTime userBirthday

--- Program.cs
+++ Program.cs
@@ -1,19 +1,27 @@
-﻿using Microsoft.Win32;
-using System;
-using System.ComponentModel.Design;
-using System.Globalization;
-using System.Reflection;
-
-public class Example
+﻿public class Example
 {
     public static void Main()
     {
         var ageLimit = 18;
+
         Console.WriteLine("""Hello, please, input your date of birth in the format DD/MM/YYYY""");
         string userInputDate = Console.ReadLine();
-        DateTime userInputDateParsed = DateTime.Parse(userInputDate, CultureInfo.CreateSpecificCulture("en-GB"));
+
+        var str = userInputDate;
+        DateTime userBirthday;
+
+        var isValidDate = DateTime.TryParse(str, out userBirthday);
+
+        if (isValidDate)
+            Console.WriteLine("Your Birthday is " + userBirthday);
+        else
+        {
+            Console.WriteLine($"{str} is not a valid date string");
+            Console.In.ReadLine();
+        }
+
         var currentYear = int.Parse(DateTime.Now.Year.ToString());
-        var userAge = currentYear - userInputDateParsed.Year;
+        var userAge = currentYear - userBirthday.Year;
 
         if (userAge >= ageLimit)
         {
@@ -28,11 +36,11 @@ public class Example
 
             Console.WriteLine("\n" + "Summary" + "\n");
             Console.WriteLine("you have " + userAge + " years");
-            Console.WriteLine("you birthday is " + userInputDateParsed);
+            Console.WriteLine("you birthday is " + userBirthday);
             Console.WriteLine("First Name: " + userFirstName);
             Console.WriteLine("Last Name: " + userLastName);
             Console.WriteLine("Password: " + userPassword + "\n");
-            Console.WriteLine("Saved value for user input date is variable userInputDateParsed. Equals: " + userInputDateParsed + "\n");
+            Console.WriteLine("Saved value for user input date is variable DateTime userBirthday. Equals: " + userBirthday + "\n");
             Console.WriteLine("Thank you for registration. Have a nice, whatever you want :)");
 
         }
