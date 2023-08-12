e065b4beb1970dd782f41f3d99305c9271af2d17
Lyubomyr Papizh <lyubomyr.papizh@devspiration.com>
2023-08-12 15:14:14 +0300

    All Done
    1. Всі дані, які ти вводив для юзера треба перевести в клас (імʼя, дата народження і т д мають бути полями класу)
    2. Зробити список (List) юзерів (всі поля генерувати рандомно)
    3. Зробити функції пошуку юзерів по імені
    4. Зробити фунцію сортування юзерів по даті народження

--- Program.cs
+++ Program.cs
@@ -1,15 +1,10 @@
-﻿using OfficeOpenXml.FormulaParsing.Excel.Functions.Information;
-using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
-using OfficeOpenXml.Style;
-using System;
-
-namespace ConsoleAppLoginTest;
+﻿namespace ConsoleAppLoginTest;
 
 class ProgramAboutUsers
 {
     static void Main()
     {
-        var UsersNumber = new Random().Next(2, 10);
+        var UsersNumber = new Random().Next(3, 5);
         var myUsersList = new List<UsersSet>();
         for (var i = 0; i < UsersNumber; i++)
         {
@@ -17,25 +12,7 @@ class ProgramAboutUsers
             user.GenerateRandomValuesForUser();
             myUsersList.Add(user);
         }
-        //Console.In.ReadLine();
-
-        for (var i = 0; i < UsersNumber; i++)
-        {            
-            Console.WriteLine("\n" + "User number " + i);
-            Console.WriteLine(value: $"First name is   {myUsersList[i].UserFirstName}.");
-            Console.WriteLine(value: $"Last name is    {myUsersList[i].UserLastName}.");
-            Console.WriteLine(value: $"Password is     {myUsersList[i].UserPassword}.");
-            Console.WriteLine(value: $"UserBirthday is {myUsersList[i].UserBirthday}.");
-        }
-
-        Console.In.ReadLine();
-
-        //List<Order> SortedList = myUsersList.OrderBy(UserFirstName).ToList();
-        //myUsersList.Sort(myUsersList(Delegate).UserFirstName);
-             
-
-
-
+        Console.WriteLine("Unsorted users list");
         for (var i = 0; i < UsersNumber; i++)
         {
             Console.WriteLine("\n" + "User number " + i);
@@ -45,6 +22,55 @@ class ProgramAboutUsers
             Console.WriteLine(value: $"UserBirthday is {myUsersList[i].UserBirthday}.");
         }
 
+        Console.WriteLine("\n" + "Hello, please, input (1) to see userslist sorted by Name" +
+                          "\n" + "Hello, please, input (2) to see userslist sorted by Birthday");
+        var sortBy = int.Parse(Console.ReadLine().ToString());
+        if (sortBy == 1)
+        {
+            myUsersList.Sort((x, y) => x.UserFirstName.CompareTo(y.UserFirstName));
+
+            //не знаю як передати в попередню стрічку замість UserFirstName текстовае значення нової змінної
+            //яка би приймала різні значення, в залежності від вводу користувача
+
+            Console.WriteLine("\n" + "Users list sorted by User Name");
+        }
+        else if (sortBy == 2)
+        {
+            myUsersList.Sort((x, y) => x.UserBirthday.CompareTo(y.UserBirthday));
+            Console.WriteLine("\n" + "Users list sorted by User Birthday");
+        }
+        else
+        {
+            myUsersList.Sort((x, y) => x.UserFirstName.CompareTo(y.UserFirstName));
+            Console.WriteLine("\n" + "Incorrect Input" + "\n" + "\n" + "Show you Users list " +
+                "sorted by User First Name");
+        }
+
+        for (var i = 0; i < UsersNumber; i++)
+        {
+            Console.WriteLine("\n" + "User number " + i);
+            Console.WriteLine(value: $"First name is   {myUsersList[i].UserFirstName}");
+            Console.WriteLine(value: $"Last name is    {myUsersList[i].UserLastName}");
+            Console.WriteLine(value: $"Password is     {myUsersList[i].UserPassword}");
+            Console.WriteLine(value: $"UserBirthday is {myUsersList[i].UserBirthday}");
+        }
+
+        Console.WriteLine("\n" + "Input User First Name for Search");
+        string userNameForSearch = Console.ReadLine();
+        for (var i = 0; i < UsersNumber; i++)
+        {
+            if (userNameForSearch == myUsersList[i].UserFirstName)
+            {
+                Console.WriteLine("\n" + "Search result" + "\n" + "User number " + i);
+                Console.WriteLine(value: $"First name is   {myUsersList[i].UserFirstName}");
+                Console.WriteLine(value: $"Last name is    {myUsersList[i].UserLastName}");
+                Console.WriteLine(value: $"Password is     {myUsersList[i].UserPassword}");
+                Console.WriteLine(value: $"UserBirthday is {myUsersList[i].UserBirthday}");
+            }
+        }
+        Console.WriteLine("\n" + "If you see the search results, you are presented with all " +
+    "users with the First Name <" + userNameForSearch + "> in the list" + "\n" +
+    "Else, unfortunately, there is no user with such First Name");
         Console.In.ReadLine();
     }
 }
